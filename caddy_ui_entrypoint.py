483f82ad7e9fe16ecd7c6e3aeee2d597ba0e4b76
Juloc <juli.hiresch@gmail.com>
2026-07-27 20:51:04 +0200

    Enable multidimensional login protection

--- caddy_ui_entrypoint.py
+++ caddy_ui_entrypoint.py
@@ -51,6 +51,9 @@ def main() -> int:
         from caddy_ui.runtime_security import install as install_runtime_security
 
         install_runtime_security()
+        from caddy_ui.login_security import install as install_login_security
+
+        install_login_security()
         from caddy_ui.public_auth import main as web_main
 
         return web_main()
