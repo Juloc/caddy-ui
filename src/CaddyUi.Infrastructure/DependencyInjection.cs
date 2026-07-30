481f73fa354921b4bccc2f9e2f7ba18d3c1f4047
Juloc <juli.hiresch@gmail.com>
2026-07-31 01:20:09 +0200

    feat: register ACME email service

--- src/CaddyUi.Infrastructure/DependencyInjection.cs
+++ src/CaddyUi.Infrastructure/DependencyInjection.cs
@@ -54,6 +54,7 @@ public static class DependencyInjection
         services.AddSingleton<LoginProtectionService>();
         services.AddSingleton<DomainProviderStore>();
         services.AddSingleton<CertificateStatusService>();
+        services.AddSingleton<AcmeEmailService>();
         services.AddSingleton<RouteManagementStore>();
         services.AddSingleton<RouteImportStore>();
         services.AddSingleton<RouteTransferService>();
