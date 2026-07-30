c2731c955f0f5a0dc0d8c7204bf0119b15527204
Juloc <juli.hiresch@gmail.com>
2026-07-31 01:02:17 +0200

    fix: normalize remote Caddy admin address

--- scripts/caddy-remote.sh
+++ scripts/caddy-remote.sh
@@ -3,8 +3,12 @@ set -eu
 
 if [ "${1:-}" = "reload" ]; then
     shift
+    admin_address="${CADDY_ADMIN_URL:-caddy:2019}"
+    admin_address="${admin_address#http://}"
+    admin_address="${admin_address#https://}"
+
     exec /usr/bin/caddy reload \
-        --address "${CADDY_ADMIN_URL:-http://caddy:2019}" \
+        --address "$admin_address" \
         "$@"
 fi
 
