04456c5397da2e15fcc4b609815bef812fdc8b5f
Juloc <juli.hiresch@gmail.com>
2026-08-01 17:26:22 +0200

    release: Caddy UI 2.1.15
    * release: set version 2.1.15
    
    * release: set container version 2.1.15
    
    * release: set product version 2.1.15
    
    * test: expect product version 2.1.15

--- src/CaddyUi.Domain/ProductMetadata.cs
+++ src/CaddyUi.Domain/ProductMetadata.cs
@@ -3,5 +3,5 @@ namespace CaddyUi.Domain;
 public static class ProductMetadata
 {
     public const string Name = "Caddy UI";
-    public const string FoundationVersion = "2.1.14";
+    public const string FoundationVersion = "2.1.15";
 }
