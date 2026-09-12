c0d914863ba92949cd687be22c077fa38be7ba5a
Juloc <juli.hiresch@gmail.com>
2026-09-12 13:25:05 +0200

    Register health operations store

--- src/CaddyUi.Infrastructure/DependencyInjection.cs
+++ src/CaddyUi.Infrastructure/DependencyInjection.cs
@@ -74,6 +74,7 @@ public static class DependencyInjection
         services.AddSingleton<DnsOperationsStore>();
         services.AddSingleton<NotificationOperationsStore>();
         services.AddSingleton<ScheduledJobOperationsStore>();
+        services.AddSingleton<HealthOperationsStore>();
         services.AddSingleton<SecretReferenceResolver>();
         services.AddSingleton<ISecretReferenceResolver>(serviceProvider =>
             serviceProvider.GetRequiredService<SecretReferenceResolver>());
@@ -128,7 +129,7 @@ public static class DependencyInjection
         services.AddHostedService<AnalyticsIngestionWorker>();
         services.AddHostedService<AnalyticsMaintenanceWorker>();
         services.AddSingleton(ipSecurityOptions);
-        services.AddSingleton(TimeProvider.System);
+        services.AddSingleton<TimeProvider>(TimeProvider.System);
         services.AddSingleton<IpAddressClassifier>();
         services.AddSingleton<ClientRiskEngine>();
         services.AddSingleton<IpIntelligenceStore>();
